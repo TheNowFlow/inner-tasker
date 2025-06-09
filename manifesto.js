5bc068da40f89ec9b00653c5f79ddf7aa800a36f
TheNowFlow <thenowflow@gmail.com>
2025-06-09 08:49:44 -0400

    Delete manifesto.js

--- manifesto.js
+++
@@ -1,4 +0,0 @@
-// Kai Constructor — InnerTasker v0.1
-// Este archivo contiene la intención del proyecto:
-// Ser una herramienta que une productividad consciente con propósito evolutivo.
-// Aquí comienza el viaje. Todo lo demás se construye desde aquí.
